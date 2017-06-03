3b1556bf67d943567d68061f379d08b74b5a30bd
sd-adams <stephen_adams@cox.net>
2017-06-03 12:48:43 -0700

    updated start-all.sh

--- start-all.sh
+++ start-all.sh
@@ -3,3 +3,4 @@ nohup mvn -f ./authserver/pom.xml spring-boot:run &
 nohup mvn -f ./edgeserver/pom.xml spring-boot:run &
 nohup mvn -f ./orderservice/pom.xml spring-boot:run &
 nohup mvn -f ./orderendpoint/pom.xml spring-boot:run &
+nohup mvn -f ./receiveorderservice/pom.xml spring-boot:run &
